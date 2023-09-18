fec66a05b7b39144c790789f7c6f0451ea0e0218
julkwel <jrajerison@bocasay.com>
2023-09-18 09:05:07 +0300

    add files setup

---
+++ assets/theme/entry_script.js
@@ -0,0 +1,9 @@
+
+require('./vendor/aos/aos.js')
+require('./vendor/purecounter/purecounter_vanilla.js')
+require('./vendor/bootstrap/js/bootstrap.bundle.min.js')
+require('./vendor/glightbox/js/glightbox.min.js')
+require('./vendor/isotope-layout/isotope.pkgd.min.js')
+require('./vendor/swiper/swiper-bundle.min.js')
+require('./vendor/php-email-form/validate.js')
+require('./js/main.js')
