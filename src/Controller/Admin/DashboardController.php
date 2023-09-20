5a9c950f59d0533d72fa145e73ffedb72f6650be
julkwel <jrajerison@bocasay.com>
2023-09-20 19:09:23 +0300

    Directory structure

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,7 +4,7 @@
  * Date : 19/09/2023
  */
 
-namespace App\Controller\Dashboard;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
--- src/Controller/Middle/RegistrationController.php
+++ src/Controller/Middle/RegistrationController.php
@@ -1,26 +1,21 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Middle;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use App\Security\DtcAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(
-        Request $request,
-        UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager
-    ): Response {
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -33,7 +28,7 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            
+
             return $this->redirectToRoute('admin_dashboard_index');
         }
 
