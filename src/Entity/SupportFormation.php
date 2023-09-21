694cf166a023cb7bc9f089eac8e75a9de933850e
julkwel <jrajerison@bocasay.com>
2023-09-21 06:35:12 +0300

    Add extra tables /// WIP

--- src/Entity/SupportFormation.php
+++ src/Entity/SupportFormation.php
@@ -4,11 +4,16 @@ namespace App\Entity;
 
 use App\Repository\SupportFormationRepository;
 use Doctrine\DBAL\Types\Types;
+use Doctrine\Migrations\Tools\Console\Exception\FileTypeNotSupported;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SupportFormationRepository::class)]
 class SupportFormation
 {
+    public const VIDEO_TYPE = 1;
+    public const PDF_TYPE = 2;
+    public const IMG_TYPE = 3;
+    public const OTHER_TYPE = 4;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -19,6 +24,9 @@ class SupportFormation
 
     #[ORM\ManyToOne]
     private ?Cohorte $formation = null;
+
+    #[ORM\Column]
+    private ?int $type = null;
 
     public function getId(): ?int
     {
@@ -47,5 +55,25 @@ class SupportFormation
         $this->formation = $formation;
 
         return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): static
+    {
+        if (!in_array($type, self::getAllType())) {
+            throw new \App\Exception\FileTypeNotSupported("File not supported");
+        }
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public static function getAllType()
+    {
+        return [self::VIDEO_TYPE, self::PDF_TYPE, self::OTHER_TYPE, self::IMG_TYPE];
     }
 }
---
+++ src/Exception/FileTypeNotSupported.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author jul
+ * Date : 21/09/2023
+ */
+
+namespace App\Exception;
+
+
+class FileTypeNotSupported extends \BadMethodCallException
+{
+}
