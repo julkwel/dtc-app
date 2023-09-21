c8c3ad9613e54eca66e1674c8b3092f88322d305
julkwel <jrajerison@bocasay.com>
2023-09-21 05:47:44 +0300

    user data

---
+++ src/Controller/Middle/StudentProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\Middle;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/student', name: 'dt_student_')]
+class StudentProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'profile')]
+    public function index(): Response
+    {
+        return $this->render('middle/student_profile/index.html.twig', [
+            'student' => $this->getUser(),
+        ]);
+    }
+}
