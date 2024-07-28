dec6314af163e93a7defaec467a218ff1142c4e7
julkwel <jrajerison@bocasay.com>
2024-07-28 13:50:56 +0300

    Design (WIP)

--- src/Controller/Front/DtcHomePageController.php
+++ src/Controller/Front/DtcHomePageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Front;
 
 use App\Entity\Cohorte;
+use App\Entity\StudentFormation;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(name: 'dtc_')]
 class DtcHomePageController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $entityManager)
+    public function __construct(private readonly EntityManagerInterface $entityManager)
     {
     }
 
@@ -19,10 +20,12 @@ class DtcHomePageController extends AbstractController
     public function index(): Response
     {
         $openFormation = $this->entityManager->getRepository(Cohorte::class)->findBy(['isRegisterOpen' => true]);
+        $students = $this->entityManager->getRepository(StudentFormation::class)->findBy([]);
 
         return $this->render('dtc_home_page/index.html.twig', [
             'controller_name' => '',
             'formations' => $openFormation,
+            'students' => $students
         ]);
     }
 }
